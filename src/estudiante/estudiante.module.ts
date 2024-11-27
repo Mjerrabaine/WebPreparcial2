164d732f769b3d0e4c94469bbb6f5ec85ea2debc
Mjerrabaine <af.galvis@uniandes.edu.co>
2024-11-27 12:41:39 -0500

    updated readme

--- src/estudiante/estudiante.module.ts
+++ src/estudiante/estudiante.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { EstudianteService } from './estudiante.service';
-import { EstudianteController } from './estudiante.controller';
 
 @Module({
   providers: [EstudianteService],
